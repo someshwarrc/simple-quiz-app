45589c3d367057de67483cc3995c8b0199a2200c
someshwarrc <someshwarrc@gmail.com>
2021-07-10 09:21:12 +0530

    Updates

---
+++ precache-manifest.e5fb9d83263531afe385e5ab9da14adf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2b0201a093e58bce5bec2b620a75562",
+    "url": "/simple-quiz-app/index.html"
+  },
+  {
+    "revision": "ad1f465233e9a4eecf27",
+    "url": "/simple-quiz-app/static/css/main.5a032b4e.chunk.css"
+  },
+  {
+    "revision": "add2a9b5ca5d2e053bae",
+    "url": "/simple-quiz-app/static/js/2.5c2f6845.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-quiz-app/static/js/2.5c2f6845.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad1f465233e9a4eecf27",
+    "url": "/simple-quiz-app/static/js/main.c561296a.chunk.js"
+  },
+  {
+    "revision": "ba513eff9dcaaaea3b08",
+    "url": "/simple-quiz-app/static/js/runtime-main.c929c300.js"
+  }
+]);
